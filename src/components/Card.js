2d5f9197320d41e3a97804118a20a49aa5c3ff62
Itzel Ramon <itzelramonf@gmail.com>
2023-04-15 20:07:06 -0500

    Se termino el diseño de la Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,52 @@
 import { StyleSheet, Text, View } from 'react-native';
+import TouchableIcon from './TouchableIcon';
+import { faShareFromSquare, faTrashCan, faArrowAltCircleUp, faCircleXmark} from '@fortawesome/free-regular-svg-icons';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 const Card = props => {
    const { title, origin, insertion, fuction, innervation } = props;
 
    return (
-      <View style={styles.container}>
-         <Text style={styles.title}>{title}</Text>
-         <Text style={styles.subtitle}>Origen: <Text style={styles.description}>{origin}</Text></Text>
-         <Text style={styles.subtitle}>Inserción: <Text style={styles.description}>{insertion}</Text></Text>
-         <Text style={styles.subtitle}>Función: <Text style={styles.description}>{fuction}</Text></Text>
-         <Text style={styles.lastSubtile}>Inervación: <Text style={styles.description}>{innervation}</Text></Text>
+      <View>
+         <View style={styles.bottonClose}>
+            <TouchableIcon
+               onPress={() => console.log('cerrar card')} 
+               icon={faCircleXmark}/>
+         </View>
+         <View style={styles.card}>
+            <View style={styles.titleSection}>
+               <Text style={styles.title}>{title}</Text>
+               <View style={styles.iconContainer}> 
+                  <TouchableIcon
+                     onPress={() => console.log('pruebita:)')} 
+                     icon={faArrowAltCircleUp}/>
+                  <TouchableIcon
+                     onPress={() => console.log('pruebita:)')} 
+                     icon={faShareFromSquare}/>
+                  <TouchableIcon 
+                     onPress={() => console.log('pruebita:)')} 
+                     icon={faTrashCan}/>
+               </View>
+            </View>
+            <Text style={styles.subtitle}>Origen: <Text style={styles.description}>{origin}</Text></Text>
+            <Text style={styles.subtitle}>Inserción: <Text style={styles.description}>{insertion}</Text></Text>
+            <Text style={styles.subtitle}>Función: <Text style={styles.description}>{fuction}</Text></Text>
+            <Text style={styles.lastSubtile}>Inervación: <Text style={styles.description}>{innervation}</Text></Text>
+         </View>
+         <View style={styles.arrowContainer}>
+            <TouchableIcon
+               onPress={() => console.log('izquierda')} 
+               icon={faAngleLeft}/>
+            <TouchableIcon
+               onPress={() => console.log('derecha')} 
+               icon={faAngleRight }/>
+         </View>
       </View>
    );
 };
 
 const styles = StyleSheet.create({
-   container: {
+   card: {
       paddingHorizontal: 10,
       paddingVertical: 20,
       backgroundColor:"#FFC8C8",
@@ -41,6 +72,23 @@ const styles = StyleSheet.create({
    description: {
       fontFamily: 'Inter',
       fontSize: 16,
+   },
+   iconContainer: {
+      flexDirection: "row",
+   },
+   titleSection: {
+      flexDirection: "row",
+      justifyContent: 'space-between'
+   },
+   arrowContainer: {
+      flexDirection: "row",
+      justifyContent: 'space-between',
+      marginTop: 10
+   },
+   bottonClose: {
+      flexDirection: "row",
+      justifyContent: 'flex-end',
+      marginBottom: 5
    }
 });
 
