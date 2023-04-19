ee05f23beda2845286f6f3e0f3fa07a7362b1291
Jose Guzman <joseguzmandev@gmail.com>
2023-04-19 09:38:55 -0500

    Prueba

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,51 +1,45 @@
-import React, { useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Image, Text } from 'react-native';
 import GoogleButton from '@components/GoogleButton';
 import Layout from '@components/Layout';
 import rectangularLogo from '@assets/rectangular-logo.png';
 import * as WebBrowser from 'expo-web-browser';
 import { useNavigation } from '@react-navigation/native';
-import {
-   makeRedirectUri,
-   useAuthRequest,
-   ResponseType,
-} from 'expo-auth-session';
+import * as Google from 'expo-auth-session/providers/google';
 
 WebBrowser.maybeCompleteAuthSession();
 
 const Login = () => {
+   const [accessToken, setAccessToken] = useState(null);
+   const [user, setUser] = useState(null);
    const navigation = useNavigation();
 
-   const discovery = {
-      authorizationEndpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
-      tokenEndpoint: 'https://oauth2.googleapis.com/token',
-      revocationEndpoint: 'https://oauth2.googleapis.com/revoke',
-   };
+   const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+      clientId:
+         '621833253404-kkvm74rtt7p5nkq46fdgjo6dgh21icl0.apps.googleusercontent.com',
+      scopes: ['openid', 'profile', 'email'],
+   });
 
-   const [request, response, promptAsync] = useAuthRequest(
-      {
-         clientId:
-            '621833253404-hkbp55ntsdu7anon0uaia0clfcjuflkd.apps.googleusercontent.com',
-         scopes: ['openid', 'profile', 'email'],
-         responseType: ResponseType.Token,
-         usePKCE: false,
-         redirectUri: makeRedirectUri({ useProxy: true }),
-      },
-      discovery
-   );
-
-   const handleGoogleSignIn = useCallback(async () => {
-      try {
-         console.log('Redirect URI:', makeRedirectUri({ useProxy: true }));
-         const authResult = await promptAsync();
-
-         if (authResult.type === 'success') {
-            navigation.navigate('Home');
-         }
-      } catch (error) {
-         console.log('Error during Google sign in:', error);
+   useEffect(() => {
+      if (response?.type === 'success') {
+         setAccessToken(response.authentication.accessToken);
       }
-   }, [promptAsync, navigation]);
+   }, [response]);
+
+   useEffect(() => {
+      if (accessToken) {
+         fetchUserInfo();
+      }
+   }, [accessToken]);
+
+   async function fetchUserInfo() {
+      let response = await fetch('https://www.googleapis.com/userinfo/v2/me', {
+         headers: { Authorization: `Bearer ${accessToken}` },
+      });
+      const userInfo = await response.json();
+      setUser(userInfo);
+      navigation.navigate('Navigation');
+   }
 
    return (
       <Layout>
@@ -55,7 +49,11 @@ const Login = () => {
                Inicia sesión fácilmente con tu cuenta de Google. No te
                preocupes, tus datos están completamente seguros.
             </Text>
-            <GoogleButton onPress={handleGoogleSignIn} />
+            <GoogleButton
+               onPress={() => {
+                  promptAsync();
+               }}
+            />
          </View>
       </Layout>
    );
