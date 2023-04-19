5cd20ef74ff90b0eba594c5fb5ede9b0c58fdeef
Diego Gutiérrez <diegogutcat28@gmail.com>
2023-04-19 08:32:07 -0500

    Funciones para mover y eliminar tarjetas

--- src/screens/CardList.js
+++ src/screens/CardList.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { StyleSheet, View, Image, ScrollView } from 'react-native';
+import { StyleSheet, View, Image, ScrollView, Modal, Text } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Layout from '@components/Layout';
 import TextInput from '@components/CustomTextInput';
@@ -9,11 +10,110 @@ import logo from '@assets/logo.png';
 
 import { MusclesApi } from '@api/MusclesApi';
 
-const CardList = ({ route }) => {
-   const { folder } = route.params;
-   const [cardInputValue, setCardInputValue] = useState('');
+const CardList = ({ route, navigation }) => {
+   const { folder, folders } = route.params;
    const [cards, setCards] = useState(folder.cards);
+   const [selectedFolderName, setSelectedFolderName] = useState('');
+   const [selectedFolder, setSelectedFolder] = useState({});
+   const [selectedCard, setSelectedCard] = useState({});
+   const [cardInputValue, setCardInputValue] = useState('');
+   const [modalVisible, setModalVisible] = useState(false);
    const { getMuscles } = MusclesApi;
+
+   const handleOpenCard = card => {
+      navigation.navigate('CardView', { card, folder, folders });
+   };
+
+   const handleSelectedFolder = async folderName => {
+      setSelectedFolderName(folderName);
+      const folder = folders.find(folder => folder.name === folderName);
+      setSelectedFolder(folder);
+   };
+
+   const handleMoveCard = async () => {
+      if (!selectedFolderName) return;
+
+      // Crear una nueva lista de tarjetas que excluya la tarjeta movida
+      const newCards = cards.filter(card => card.title !== selectedCard.title);
+
+      // Actualizar el estado con la nueva lista de tarjetas
+      setCards(newCards);
+
+      // Actualizar la propiedad "cards" de la carpeta
+      folder.cards = newCards;
+
+      try {
+         // Obtener la lista de carpetas del almacenamiento local
+         const folders = await AsyncStorage.getItem('folders');
+
+         if (folders !== null) {
+            // Convertir la lista de carpetas de una cadena JSON a un objeto JavaScript
+            const parsedFolders = JSON.parse(folders);
+
+            // Encontrar la carpeta que se actualizó en la lista de carpetas
+            const updatedFolderIndex = parsedFolders.findIndex(
+               f => f.name === folder.name,
+            );
+            parsedFolders[updatedFolderIndex] = folder;
+
+            // Guardar la lista actualizada de carpetas en el almacenamiento local
+            await AsyncStorage.setItem(
+               'folders',
+               JSON.stringify(parsedFolders),
+            );
+
+            setCards(folder.cards);
+         }
+      } catch (error) {
+         console.error('Error updating folders:', error);
+      }
+
+      // Agregar la nueva tarjeta a la propiedad "cards" de la carpeta
+      selectedFolder.cards.push(selectedCard);
+
+      // Guardar la lista actualizada de carpetas en el almacenamiento local
+      await AsyncStorage.setItem('folders', JSON.stringify(folders));
+
+      setModalVisible(false);
+   };
+
+   const handleDeleteCard = async card => {
+      // Crear una nueva lista de tarjetas que excluya la tarjeta eliminada
+      const newCards = cards.filter(c => c.title !== card.title);
+
+      // Actualizar el estado con la nueva lista de tarjetas
+      setCards(newCards);
+
+      // Actualizar la propiedad "cards" de la carpeta
+      folder.cards = newCards;
+
+      try {
+         // Obtener la lista de carpetas del almacenamiento local
+         const folders = await AsyncStorage.getItem('folders');
+
+         if (folders !== null) {
+            // Convertir la lista de carpetas de una cadena JSON a un objeto JavaScript
+            const parsedFolders = JSON.parse(folders);
+
+            // Encontrar la carpeta que se actualizó en la lista de carpetas
+
+            const updatedFolderIndex = parsedFolders.findIndex(
+               f => f.name === folder.name,
+            );
+            parsedFolders[updatedFolderIndex] = folder;
+
+            // Guardar la lista actualizada de carpetas en el almacenamiento local
+            await AsyncStorage.setItem(
+               'folders',
+               JSON.stringify(parsedFolders),
+            );
+
+            setCards(folder.cards);
+         }
+      } catch (error) {
+         console.error('Error updating folders:', error);
+      }
+   };
 
    const handleCreateCard = async () => {
       const newCard = await getMuscles(cardInputValue);
@@ -44,14 +144,14 @@ const CardList = ({ route }) => {
 
             // Encontrar la carpeta que se actualizó en la lista de carpetas
             const updatedFolderIndex = parsedFolders.findIndex(
-               (f) => f.name === folder.name
+               f => f.name === folder.name,
             );
             parsedFolders[updatedFolderIndex] = folder;
 
             // Guardar la lista actualizada de carpetas en el almacenamiento local
             await AsyncStorage.setItem(
                'folders',
-               JSON.stringify(parsedFolders)
+               JSON.stringify(parsedFolders),
             );
 
             setCards(folder.cards);
@@ -65,28 +165,106 @@ const CardList = ({ route }) => {
       <Layout>
          <View style={styles.container}>
             <View style={styles.logoContainer}>
-               <Image source={logo} style={styles.image} />
+               <Image
+                  source={logo}
+                  style={styles.image}
+               />
+            </View>
+            <View style={{ marginTop: 30 }}>
+               <Text
+                  style={{
+                     fontFamily: 'Quicksand-Bold',
+                     textAlign: 'center',
+                     fontSize: 20,
+                  }}
+               >
+                  Carpeta: {folder.name}
+               </Text>
             </View>
             <View style={styles.createSection}>
                <TextInput
-                  label='Escriba el músculo'
+                  label="Escriba el músculo"
                   onChangeText={setCardInputValue}
                />
                <View style={{ height: 20 }} />
-               <TextButton color='#C6E9FB' onPress={handleCreateCard}>
+               <TextButton
+                  color="#C6E9FB"
+                  onPress={handleCreateCard}
+               >
                   Crear tarjeta
                </TextButton>
             </View>
-
             <ScrollView>
                {cards.map((card, index) => (
                   <MinimizedCard
                      title={card.title}
-                     origin={card.origin}
+                     functions={card.function}
+                     onOpen={() => handleOpenCard(card)}
+                     onMoveToFolder={() => {
+                        setSelectedCard(card);
+                        setModalVisible(true);
+                     }}
+                     onDelete={() => {
+                        handleDeleteCard(card);
+                     }}
                      key={index}
                   />
                ))}
             </ScrollView>
+            <Modal
+               animationType="slide"
+               transparent={true}
+               visible={modalVisible}
+               onRequestClose={() => setModalVisible(false)}
+            >
+               <View
+                  style={{
+                     flex: 1,
+                     alignItems: 'center',
+                     justifyContent: 'center',
+                  }}
+               >
+                  <View
+                     style={{
+                        backgroundColor: '#fff',
+                        padding: 20,
+                        borderRadius: 5,
+                        borderWidth: 2,
+                     }}
+                  >
+                     <Picker
+                        selectedValue={selectedFolderName}
+                        onValueChange={selectedValue =>
+                           handleSelectedFolder(selectedValue)
+                        }
+                     >
+                        <Picker.Item
+                           label="Seleccione una carpeta"
+                           value=""
+                        />
+                        {folders.map((item, index) => {
+                           if (item.name !== folder.name) {
+                              return (
+                                 <Picker.Item
+                                    label={item.name}
+                                    value={item.name}
+                                    key={index}
+                                 />
+                              );
+                           }
+                           return null;
+                        })}
+                     </Picker>
+                     <View style={{ marginBottom: 15 }} />
+                     <TextButton
+                        color="#C6E9FB"
+                        onPress={handleMoveCard}
+                     >
+                        Mover
+                     </TextButton>
+                  </View>
+               </View>
+            </Modal>
          </View>
       </Layout>
    );
@@ -106,7 +284,7 @@ const styles = StyleSheet.create({
       height: 50,
    },
    createSection: {
-      marginVertical: 40,
+      marginVertical: 20,
    },
    createButton: {
       marginTop: 20,
--- src/screens/CardView.js
+++ src/screens/CardView.js
@@ -1,9 +1,106 @@
-import { StyleSheet, View, Image } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Image, Modal, Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Picker } from '@react-native-picker/picker';
 import Layout from '@components/Layout';
+import TextButton from '@components/TextButton';
 import Card from '../components/Card';
 import logo from '@assets/logo.png';
 
-const CardView = () => {
+const CardView = ({ route, navigation }) => {
+   const { card, folder, folders } = route.params;
+
+   const [selectedFolderName, setSelectedFolderName] = useState('');
+   const [selectedFolder, setSelectedFolder] = useState({});
+   const [modalVisible, setModalVisible] = useState(false);
+
+   const handleCloseCard = () => {
+      navigation.navigate('Home');
+   };
+
+   const handleSelectedFolder = async folderName => {
+      setSelectedFolderName(folderName);
+      const folder = folders.find(folder => folder.name === folderName);
+      setSelectedFolder(folder);
+   };
+
+   const handleMoveCard = async () => {
+      if (!selectedFolderName) return;
+
+      // Crear una nueva lista de tarjetas que excluya la tarjeta movida
+      const newCards = folder.cards.filter(card => card.title !== card.title);
+
+      // Actualizar la propiedad "cards" de la carpeta
+      folder.cards = newCards;
+
+      try {
+         // Obtener la lista de carpetas del almacenamiento local
+         const folders = await AsyncStorage.getItem('folders');
+
+         if (folders !== null) {
+            // Convertir la lista de carpetas de una cadena JSON a un objeto JavaScript
+            const parsedFolders = JSON.parse(folders);
+
+            // Encontrar la carpeta que se actualizó en la lista de carpetas
+            const updatedFolderIndex = parsedFolders.findIndex(
+               f => f.name === folder.name,
+            );
+            parsedFolders[updatedFolderIndex] = folder;
+
+            // Guardar la lista actualizada de carpetas en el almacenamiento local
+            await AsyncStorage.setItem(
+               'folders',
+               JSON.stringify(parsedFolders),
+            );
+         }
+      } catch (error) {
+         console.error('Error updating folders:', error);
+      }
+
+      // Agregar la nueva tarjeta a la propiedad "cards" de la carpeta
+      selectedFolder.cards.push(card);
+
+      // Guardar la lista actualizada de carpetas en el almacenamiento local
+      await AsyncStorage.setItem('folders', JSON.stringify(folders));
+
+      setModalVisible(false);
+      handleCloseCard();
+   };
+
+   const handleDeleteCard = async () => {
+      // Crear una nueva lista de tarjetas que excluya la tarjeta eliminada
+      const newCards = folder.cards.filter(c => c.title !== card.title);
+
+      // Actualizar la propiedad "cards" de la carpeta
+      folder.cards = newCards;
+
+      try {
+         // Obtener la lista de carpetas del almacenamiento local
+         const folders = await AsyncStorage.getItem('folders');
+
+         if (folders !== null) {
+            // Convertir la lista de carpetas de una cadena JSON a un objeto JavaScript
+            const parsedFolders = JSON.parse(folders);
+
+            // Encontrar la carpeta que se actualizó en la lista de carpetas
+
+            const updatedFolderIndex = parsedFolders.findIndex(
+               f => f.name === folder.name,
+            );
+            parsedFolders[updatedFolderIndex] = folder;
+
+            // Guardar la lista actualizada de carpetas en el almacenamiento local
+            await AsyncStorage.setItem(
+               'folders',
+               JSON.stringify(parsedFolders),
+            );
+            handleCloseCard();
+         }
+      } catch (error) {
+         console.error('Error updating folders:', error);
+      }
+   };
+
    return (
       <Layout>
          <View style={styles.container}>
@@ -13,16 +110,82 @@ const CardView = () => {
                   style={styles.image}
                />
             </View>
+            <View style={{ marginTop: 30 }}>
+               <Text
+                  style={{
+                     fontFamily: 'Quicksand-Bold',
+                     textAlign: 'center',
+                     fontSize: 20,
+                  }}
+               >
+                  Carpeta: {folder.name}
+               </Text>
+            </View>
             <View style={styles.cardContainer}>
-            <Card 
-               title="Serratus Major"
-               origin="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." 
-               insertion="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." 
-               fuction="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." 
-               innervation="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-            />
+               <Card
+                  data={card}
+                  onClose={handleCloseCard}
+                  onMoveToFolder={() => {
+                     setModalVisible(true);
+                  }}
+                  onDelete={handleDeleteCard}
+               />
+            </View>
          </View>
-         </View>
+         <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => setModalVisible(false)}
+         >
+            <View
+               style={{
+                  flex: 1,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+               }}
+            >
+               <View
+                  style={{
+                     backgroundColor: '#fff',
+                     padding: 20,
+                     borderRadius: 5,
+                     borderWidth: 2,
+                  }}
+               >
+                  <Picker
+                     selectedValue={selectedFolderName}
+                     onValueChange={selectedValue =>
+                        handleSelectedFolder(selectedValue)
+                     }
+                  >
+                     <Picker.Item
+                        label="Seleccione una carpeta"
+                        value=""
+                     />
+                     {folders.map((item, index) => {
+                        if (item.name !== folder.name) {
+                           return (
+                              <Picker.Item
+                                 label={item.name}
+                                 value={item.name}
+                                 key={index}
+                              />
+                           );
+                        }
+                        return null;
+                     })}
+                  </Picker>
+                  <View style={{ marginBottom: 15 }} />
+                  <TextButton
+                     color="#C6E9FB"
+                     onPress={handleMoveCard}
+                  >
+                     Mover
+                  </TextButton>
+               </View>
+            </View>
+         </Modal>
       </Layout>
    );
 };
